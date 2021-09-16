45252876ff2a532ae00d2b2e553d0fd8237df659
Adam Bennett <adam.bennett@cuvva.com>
2021-09-16 10:43:27 +0100

    Remove afterEvaluate

--- buildSrc/src/main/kotlin/dev/adambennett/gradle/plugins/ProjectConfigurationPlugin.kt
+++ buildSrc/src/main/kotlin/dev/adambennett/gradle/plugins/ProjectConfigurationPlugin.kt
@@ -8,54 +8,41 @@ import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.LibraryPlugin
 import com.android.build.gradle.TestedExtension
 import org.gradle.api.JavaVersion
-import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaLibraryPlugin
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.api.tasks.SourceSet
-import org.gradle.kotlin.dsl.closureOf
 import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.gradle.internal.Kapt3GradleSubplugin
 
 class ProjectConfigurationPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        project.afterEvaluate {
-            configureForAllProjects()
+        project.configureForAllProjects()
 
-            if (project == rootProject) {
-                configureForRootProject()
-            }
+        if (project.isRoot) {
+            project.configureForRootProject()
+        }
 
-            plugins.all {
-                when (this) {
-                    is JavaPlugin,
-                    is JavaLibraryPlugin,
-                    -> {
-                        project.extensions.getByType(JavaPluginExtension::class.java).apply {
-                            sourceCompatibility = JavaVersion.VERSION_1_8
-                            targetCompatibility = JavaVersion.VERSION_1_8
-
-                            sourceSets {
-                                map { it.java.srcDir("src/${it.name}/kotlin") }
-                            }
-                        }
+        project.plugins.all {
+            when (this) {
+                is JavaPlugin,
+                is JavaLibraryPlugin,
+                -> {
+                    project.extensions.getByType(JavaPluginExtension::class.java).apply {
+                        sourceCompatibility = JavaVersion.VERSION_11
+                        targetCompatibility = JavaVersion.VERSION_11
                     }
-                    is LibraryPlugin -> extensions.getByType<TestedExtension>()
-                        .configureCommonAndroidOptions()
-                    is AppPlugin -> extensions.getByType<TestedExtension>()
-                        .configureCommonAndroidOptions()
-                    is Kapt3GradleSubplugin -> configureKapt()
                 }
+                is LibraryPlugin -> project.extensions.getByType<TestedExtension>()
+                    .configureCommonAndroidOptions()
+                is AppPlugin -> project.extensions.getByType<TestedExtension>()
+                    .configureCommonAndroidOptions()
+                is Kapt3GradleSubplugin -> project.configureKapt()
             }
         }
     }
 }
 
-private fun JavaPluginExtension.sourceSets(
-    action: NamedDomainObjectContainer<SourceSet>.() -> Unit,
-) {
-    sourceSets(closureOf<NamedDomainObjectContainer<SourceSet>> { action() })
-}
+val Project.isRoot get() = this == this.rootProject
